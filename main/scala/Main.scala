3f7d71347efdcbcd06bc3216c28ff5093bd1d343
Salvador <70590860+salvador-palma@users.noreply.github.com>
2023-03-22 09:55:01 +0000

    testes

--- main/scala/Main.scala
+++ main/scala/Main.scala
@@ -1,3 +1,13 @@
-class Main {
+type Board = List[List[Cells.cell]]
+object Cells extends Enumeration {
+  type Cell = Value
+  val Red, Blue, Empty = Value
 
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+  }
 }
