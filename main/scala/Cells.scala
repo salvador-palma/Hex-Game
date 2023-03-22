e7761a2fb91a29757e4318a419143b3f8f433dae
Salvador <70590860+salvador-palma@users.noreply.github.com>
2023-03-22 11:00:51 +0000

    board print

---
+++ main/scala/Cells.scala
@@ -0,0 +1,11 @@
+package Projeto
+
+object Cells extends Enumeration {
+  type Cell = Value
+  val Red, Blue, Empty = Value
+}
+/*
+case class Cell(symb : String){
+  override def toString: String = symb
+}*/
+
--- main/scala/Main.scala
+++ main/scala/Main.scala
@@ -1,13 +1,34 @@
-type Board = List[List[Cells.cell]]
-object Cells extends Enumeration {
-  type Cell = Value
-  val Red, Blue, Empty = Value
+package Projeto
+import Projeto.Cells
 
-}
-
-object Main {
-
+object Main{
+  type Board = List[List[Cells.Cell]]
   def main(args: Array[String]): Unit = {
+    val board: Board = defineBoard(5)
+    printRow(board)
+    print(board.size)
+  }
 
+  def defineBoard(n:Integer):Board=List.fill(n)(List.fill(n)(Cells.Empty))
+  def printRow(board : Board, column : Integer = 0): Any = {
+    def printItem(row : Integer = 0):Any={
+      if(row < board.size){
+        board(column)(row) match {
+          case Cells.Empty => print(".")
+          case Cells.Blue => print("O")
+          case Cells.Red => print("X")
+        }
+        printItem(row+1)
+      }
+    }
+
+    if(column<board.size){
+      printItem()
+      println("")
+      printRow(board, column+1)
+    }
   }
 }
+
+
+
