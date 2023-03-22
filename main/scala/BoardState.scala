bcd7aa54f966b3a8a4dc111fc59e7214b287cf04
Salvador Palma <70590860+salvador-palma@users.noreply.github.com>
2023-03-22 22:35:03 +0000

    board case class and boardstate

---
+++ main/scala/BoardState.scala
@@ -0,0 +1,36 @@
+package Projeto
+import Projeto.Cells.Cell
+import Projeto.Main._
+
+case class BoardState(board:Board){
+  def drawGameState():Unit = BoardState.drawBoard(board)
+
+}
+
+object BoardState{
+  def defineBoard(n:Integer):Board=List.fill(n)(List.fill(n)(Cells.Empty))
+  def drawBoard(board: Board): Unit = {
+    println(s" ${Blue}*${Reset}  " * board.size)
+    printRows()
+    println((" " * (board.size + 1))+ s" ${Blue}*${Reset}  " * board.size)
+    def printRows(column: Integer = 0): Any = {
+      def printItems(row: Integer = 0): Any = {
+        if (row < board.size) {
+          print(board(column)(row))
+          if (row + 1 < board.size) print(" - ")
+          printItems(row + 1)
+        }
+      }
+      if (column < board.size) {
+        print((" " * column) + s"${Red}*${Reset} ");
+        printItems();
+        println(s" ${Red}*${Reset}")
+        if (column + 1 < board.size) println((" " * (column + 2)) + ("\\ / " * (board.size - 1)) + "\\")
+        printRows(column + 1)
+      }
+    }
+  }
+
+}
+
+
--- main/scala/Cells.scala
+++ main/scala/Cells.scala
@@ -1,11 +1,13 @@
 package Projeto
 
+import Projeto.Cells.Cell
+
 object Cells extends Enumeration {
   type Cell = Value
-  val Red, Blue, Empty = Value
+  val Red = Value("X")
+  val Empty = Value(".")
+  val Blue = Value("O")
 }
-/*
-case class Cell(symb : String){
-  override def toString: String = symb
-}*/
+
+
 
