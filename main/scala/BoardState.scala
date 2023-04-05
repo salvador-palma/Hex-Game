e47ab75c93b9e055b259adb556f2d4a7b6899b41
Vicente <vicentemeneseslaia@gmail.com>
2023-04-06 00:53:26 +0100

    Adjacent CPU
    Add a function in main updatePvCAdjacent
    BoardState play CPU adjacent

--- main/scala/BoardState.scala
+++ main/scala/BoardState.scala
@@ -1,51 +1,88 @@
 package Projeto
-import Projeto.BoardState.{inBounds, isEmptySlot}
+import Projeto.BoardState.{getRandomInput, inBounds, isEmptySlot}
 import Projeto.Cells.Cell
 import Projeto.Main._
 import Projeto.Random._
+
 import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.io.StdIn.readLine
 import mutable.HashMap
 
 case class BoardState(board:Board,unionFind: UnionFind){
-  def drawGameState():Unit = BoardState.drawBoard(board)
-  def drawGameStateFold():String = BoardState.drawBoardFold(board)
-  def playGameState(coords:(Int,Int), piece : Cell):BoardState = BoardState.inputBoard(this,coords, piece)
-  def getCheckInput():(Int,Int)=BoardState.retrieveInput(board)
-  def getWin(): Option[String]=this.unionFind.percolates(board)
-
-  def playCPUGameState(randomState: RandomState):(BoardState,RandomState) = BoardState.randomMove(this,randomState)
+  def draw():Unit = BoardState.drawBoard(board)
+  def drawFold():String = BoardState.drawBoardFold(board)
+  def playGameState(coords:(Int,Int), piece : Cell):BoardState = BoardState.play(this,coords, piece)
+  def getInput():(Int,Int)=BoardState.getInput(board)
+  def playCPUGameStateAdjacent(randomState: RandomState,oldCoord : (Int,Int)):((Int,Int),RandomState)= BoardState.adjacentCoord(oldCoord,this,randomState)
+  def hasContinuousLine(): Option[String]=unionFind.percolates(board)
+  def playCPUGameState(randomState: RandomState):((Int,Int),RandomState)= BoardState.getRandomInput(this,randomState)
+  def valid(input:(Int,Int)):Boolean= BoardState.inBounds(Array(input._1 + 1,input._2 + 1),board) && BoardState.isEmptySlot(input,board)
 }
 
 object BoardState{
   val BlueAst = s" ${Blue}*${Reset}  "
   val RedAst = s" ${Red}*${Reset}  "
   def defineBoard(n:Integer):Board=List.fill(n)(List.fill(n)(Cells.Empty))
-  @tailrec private def retrieveInput(board: Board):(Int,Int)={
+  @tailrec private def getInput(board: Board):(Int,Int)={
     try{
       println("Where to play?")
       readLine.trim match {
-        case "Q" => println("Thank you for playing!"); sys.exit(0)
+        case "Q" => (-1,-2)
+        case "U" => (-1,-1)
         case str => val s = str.split(" ").map(x => x.toInt); if (inBounds(s, board) && isEmptySlot((s(0)-1,s(1)-1), board)) (s(0)-1, s(1)-1) else throw new IllegalArgumentException
       }
     }catch{
       case _ => println(s"${Main.Red}Invalid Input or Coordinate already marked, try again.${Main.Reset}")
-        retrieveInput(board)
+        getInput(board)
     }
   }
-  @tailrec private def randomMove(boardState: BoardState, rand: RandomState): (BoardState, RandomState) = {
+  @tailrec private def getRandomInput(boardState: BoardState, rand: RandomState): ((Int, Int),RandomState) = {
     val x : ((Int,Int),RandomState) = rand.nextCoords(boardState.board.size)
-    if (isEmptySlot(x._1, boardState.board)){
-      (inputBoard(boardState, (x._1._2, x._1._1), Cells.Red),x._2)
-    }
-    else randomMove(boardState, x._2)
+    if (isEmptySlot(x._1, boardState.board)) x
+    else getRandomInput(boardState, x._2)
   }
-  private def inputBoard(boardState: BoardState, coords:(Int,Int), piece:Cell):BoardState={
+
+  //funcao que recebe uma coord e devolve uma adjacente
+
+  private def adjacentCoord(coord : (Int,Int) , boardState: BoardState, rand : RandomState) : ((Int,Int),RandomState) = {
+    if (coord == (-1,-1)){
+      val t = getRandomInput(boardState, rand)
+      println(t)
+      t
+    }
+    else {
+      val x = coord._1
+      val y = coord._2
+      val adjList: List[(Int, Int)] = List((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x - 1, y + 1), (x + 1, y - 1))
+      val checkAdj = (adjList foldRight true) ( (e,acc) => !boardState.valid(e) && acc)
+      if (checkAdj) {
+        adjacentCoord((-1,-1),boardState, rand)
+      }
+      else{
+        val nextRand = rand.nextInt(adjList.size)
+        val randomCoord = adjList(nextRand._1)
+        if (boardState.valid(randomCoord)) (randomCoord, nextRand._2) else adjacentCoord(coord, boardState, nextRand._2)
+      }
+    }
+  }
+/*
+  private def getBestMove() : (Int,Int) = {
+
+  }
+
+
+  private def getMinMaxInput(boardState: BoardState): (Int,Int)= {
+    if ()
+
+
+  }
+*/
+  private def play(boardState: BoardState, coords:(Int,Int), player:Cell):BoardState={
     val b : Board = boardState.board
     val u : UnionFind = boardState.unionFind
 
-    BoardState(b.updated(coords._1, b(coords._1).updated(coords._2, piece)), u.createUnions((coords._2, coords._1), boardState.board, piece))
+    BoardState(b.updated(coords._1, b(coords._1).updated(coords._2, player)), u.createUnions((coords._2, coords._1), boardState.board, player))
   }
   private def drawBoard(board: Board): Unit = {
     println(s" ${Blue}*${Reset}  " * board.size)
@@ -73,16 +110,6 @@ object BoardState{
   }
   private def inBounds(coords : Array[Int], board: Board):Boolean=coords(0) <= board.size && coords(1) <= board.size && coords(0)>0 && coords(1)>0
   private def isEmptySlot(coords:(Int,Int), board:Board):Boolean=board(coords._2)(coords._1).equals(Cells.Empty)
-
-  /*@tailrec def retrieveCPUInput(board: Board, random: Random)={
-
-  }*/
-
-
-
-
-
-
 
 }
 
--- main/scala/Main.scala
+++ main/scala/Main.scala
@@ -1,5 +1,6 @@
 package Projeto
 import Projeto.BoardState._
+import Projeto.Cells.Cell
 import Projeto.Main.Board
 
 import scala.annotation.tailrec
@@ -11,42 +12,89 @@ object Main{
   val Blue = "\u001b[34m"
   val Reset = "\u001b[0m"
   val Yellow = "\u001B[33m"
+  val Bold = "\u001B[1m"
+  val Green = "\u001B[32m"
   def main(args: Array[String]): Unit = {
-
-
-    val boardState:BoardState = BoardState(BoardState.defineBoard(5), UnionFind(UnionFind.init(5)))
-    update(boardState, Random(10))
-
+    mainMenu()
+  }
+  @tailrec def mainMenu(){
+    println(s"      ${Bold}${Red}HEX${Reset}-${Blue}GAME!${Reset}\n\n${Bold}     Main  Menu${Reset}\n\n1-Player vs. CPU (easy)\n2-Player vs. Player\nQ-Quit")
+    readLine.trim.toUpperCase match{
+      case "1"=>updatePvC(BoardState(BoardState.defineBoard(5), UnionFind(UnionFind.init(5))), Random(10));mainMenu
+      case "2"=>updatePvP(BoardState(BoardState.defineBoard(5), UnionFind(UnionFind.init(5))));mainMenu
+      case "3"=>updatePvCAdjacent(BoardState(BoardState.defineBoard(5), UnionFind(UnionFind.init(5))), Random(10));mainMenu
+      case "Q"=>println("\n See you next time!");
+      case _  =>println("Invalid Option!");mainMenu
+    }
 
   }
-/*
-  @tailrec def mainMenu(): Unit = {
-    try{
-      println(s"${Yellow}Welcome to Hex-Game!${Reset}\n\n1-Player vs. CPU\n2-Player vs. Player\nQ-Quit")
-      readLine.trim match{
-        case "1"=>update(BoardState(BoardState.defineBoard(5), UnionFind(UnionFind.init(5))))
-        case "2"=>
-        case _  =>
+  @tailrec def updatePvC(boardState: BoardState, randomState: RandomState, history:List[BoardState] = List.empty[BoardState]){
+    boardState.draw
+    boardState.getInput match {
+      case (-1,-2)=>{println(s"${Yellow}Thank you for playing!${Reset}")}
+      case (-1,-1)=> {
+        history match{
+          case Nil=>println(s"${Red}There are no moves to Undo${Reset}");updatePvC(boardState,randomState, history)
+          case h::t=>println(s"${Green}Move undone${Reset}");updatePvC(h,randomState,t)
+        }
+      }
+      case (x,y)=>  {
+        val playerBoardState: BoardState = boardState.playGameState((y, x), Cells.Blue)
+        val ((cpuX,cpuY), nextRand) = playerBoardState.playCPUGameState(randomState)
+        val CPUBoardState: BoardState = playerBoardState.playGameState((cpuY,cpuX), Cells.Red)
+        CPUBoardState.hasContinuousLine match {
+          case Some("P1") => playerBoardState.draw; println(s"${Yellow}Conratulations, You Won!!!${Reset}")
+          case Some("P2") => CPUBoardState.draw; println(s"${Yellow}Oh no, you lost to CPU :(${Reset}")
+          case _ => updatePvC(CPUBoardState, nextRand, boardState::history)
+        }
       }
     }
-    mainMenu
-  }*/
-  @tailrec def update(boardState: BoardState, randomState: RandomState){
-    boardState.drawGameState
-    val (x,y) = boardState.getCheckInput
+  }
 
-    val playerBoardState : BoardState = boardState.playGameState((y,x), Cells.Blue)
-    val winP = playerBoardState.getWin
-    if (winP != None) println(s"${Yellow}Conratulations, You Won!!!${Reset}")
-    //playerBoardState.drawGameState
+  @tailrec def updatePvCAdjacent(boardState: BoardState, randomState: RandomState, history: List[BoardState] = List.empty[BoardState], oldCoord : (Int,Int) = (-1,-1)) {
+    boardState.draw
+    boardState.getInput match {
+      case (-1, -2) => {
+        println(s"${Yellow}Thank you for playing!${Reset}")
+      }
+      case (-1, -1) => {
+        history match {
+          case Nil => println(s"${Red}There are no moves to Undo${Reset}"); updatePvC(boardState, randomState, history)
+          case h :: t => println(s"${Green}Move undone${Reset}"); updatePvC(h, randomState, t)
+        }
+      }
+      case (x, y) => {
+        val playerBoardState: BoardState = boardState.playGameState((y, x), Cells.Blue)
+        val ((cpuX, cpuY), nextRand) = playerBoardState.playCPUGameStateAdjacent(randomState,oldCoord)
+        val CPUBoardState: BoardState = playerBoardState.playGameState((cpuY, cpuX), Cells.Red)
+        CPUBoardState.hasContinuousLine match {
+          case Some("P1") => playerBoardState.draw; println(s"${Yellow}Conratulations, You Won!!!${Reset}")
+          case Some("P2") => CPUBoardState.draw; println(s"${Yellow}Oh no, you lost to CPU :(${Reset}")
+          case _ => updatePvCAdjacent(CPUBoardState, nextRand, boardState :: history,(cpuX,cpuY))
+        }
+      }
+    }
+  }
 
-    val CPUBoardState: (BoardState,RandomState) = playerBoardState.playCPUGameState(randomState)
-    val winCPU = CPUBoardState._1.getWin
-
-    if (winCPU.contains("CPU"))) println(s"${Yellow}Oh no, you lost to CPU :(${Reset}")
-    print("hello world")
-    update(CPUBoardState._1, CPUBoardState._2)
-
+  @tailrec def updatePvP(boardState: BoardState, history: List[BoardState] = List.empty[BoardState], currentPlayer : Cell = Cells.Blue) {
+    boardState.draw
+    boardState.getInput match {
+      case (-1, -2) => println(s"${Yellow}Thank you for playing!${Reset}")
+      case (-1, -1) =>
+        history match {
+          case Nil => println(s"${Red}There are no moves to Undo${Reset}"); updatePvP(boardState, history)
+          case h :: t => println(s"${Green}Move undone${Reset}"); updatePvP(h, t)
+        }
+      case (x, y) => {
+        val playerBoardState: BoardState = boardState.playGameState((y, x), currentPlayer)
+        val nextPlayer = if(currentPlayer.equals(Cells.Blue)) Cells.Red else Cells.Blue
+        playerBoardState.hasContinuousLine match {
+          case Some("P1") => playerBoardState.draw; println(s"${Yellow}Conratulations, Player 1 Won!!!${Reset}")
+          case Some("P2") => playerBoardState.draw; println(s"${Yellow}Conratulations, Player 2 Won!!!${Reset}")
+          case _ => updatePvP(playerBoardState, boardState :: history, nextPlayer)
+        }
+      }
+    }
   }
 
   }
